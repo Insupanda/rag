7896659e678e77adcf3c28ee4a3f45e70056b734
rimiyeyo <bhr0212@naver.com>
2025-06-29 13:37:08 +0900

    feat: connect to receive a response when you type in chainlit

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import logging
 
+import chainlit as cl
 from openai import OpenAI
 
 from config.logger import setup_logging
@@ -8,16 +9,23 @@ from db.sql_utils import TemplateManager
 from modules.user_state import UserState
 from services.insurance_service import InsuranceService
 
-if __name__ == "__main__":
-    setup_logging()
-    logger = logging.getLogger(__name__)
-    logging.info("\n=== 보험 상담 챗봇 ===")
+# if __name__ == "__main__":
+setup_logging()
+logger = logging.getLogger(__name__)
+logging.info("\n=== 보험 상담 챗봇 ===")
 
-    template_manager = TemplateManager(templates_dir=PROJECT_ROOT / "prompts")
-    openai_client = OpenAI(api_key=settings.openai_api_key)
-    user_state = UserState()
+template_manager = TemplateManager(templates_dir=PROJECT_ROOT / "prompts")
+openai_client = OpenAI(api_key=settings.openai_api_key)
+user_state = UserState()
 
-    insurance_service = InsuranceService(
-        openai_client=openai_client, template_manager=template_manager, user_state=user_state
-    )
-    insurance_service.run()
+insurance_service = InsuranceService(
+    openai_client=openai_client, template_manager=template_manager, user_state=user_state
+)
+
+
+@cl.on_message
+async def main(message: cl.Message):
+
+    await cl.Message(
+        content=f"Insupanda Bot: {insurance_service.get_user_response(message.content)}",
+    ).send()
