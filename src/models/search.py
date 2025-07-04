62e501e4fc4a18bc57911fcff569cf3f1595e357
rimiyeyo <bhr0212@naver.com>
2025-07-04 14:33:04 +0900

    fix: test7

--- src/models/search.py
+++ src/models/search.py
@@ -4,12 +4,11 @@ import faiss
 import numpy as np
 from numpy.typing import NDArray
 
-from config.settings import Settings
+from config.settings import settings
 from models.dict_types import DocId, DocIDMetadata, OrganizedCollection, RawCollection
 from models.embeddings import UpstageEmbedding
 
 InsuFileNames = str
-settings = Settings()
 upembedding = UpstageEmbedding(settings.upstage_api_key)
 
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def mock_env_file(monkeypatch, tmp_path):
     test_env = tmp_path / "test.env"
     test_env.write_text(
