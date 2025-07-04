c48f16f66993c74cb55aba0ef0b77e1399dd8698
rimiyeyo <bhr0212@naver.com>
2025-07-04 13:00:14 +0900

    fix: modify settings env variables

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Optional
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -9,12 +10,12 @@ class Settings(BaseSettings):
     db_host: str = "localhost"
     db_port: str = "3306"
     db_user: str = "root"
-    db_password: str
+    db_password: Optional[str] = None
     db_database: str = "insu"
 
     vector_path: str = "insu_data"
-    openai_api_key: str
-    upstage_api_key: str
+    openai_api_key: Optional[str] = None
+    upstage_api_key: Optional[str] = None
 
     model_config = SettingsConfigDict(env_file=f"{PROJECT_ROOT}/.env", env_file_encoding="utf-8")
 
