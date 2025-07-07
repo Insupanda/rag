64abe4c689cbe32489b482130fc038982dfc9ebb
rimiyeyo <bhr0212@naver.com>
2025-07-07 16:33:43 +0900

    chore: remove unnecessary commet

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,5 @@
 import warnings
 
-# Suppress specific deprecation warnings from FAISS
 warnings.filterwarnings("ignore", category=DeprecationWarning, module="faiss.loader")
 warnings.filterwarnings("ignore", message="builtin type SwigPyPacked has no __module__ attribute")
 warnings.filterwarnings("ignore", message="builtin type SwigPyObject has no __module__ attribute")
