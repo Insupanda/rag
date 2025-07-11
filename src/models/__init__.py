e9f6e22e0fcd1cbd55f8ba48c41bb3ab3dcd7628
rimiyeyo <bhr0212@naver.com>
2025-07-11 15:27:27 +0900

    chore: remove ignore package and downgrade numpy

--- src/models/__init__.py
+++
@@ -1,6 +0,0 @@
-import warnings
-
-warnings.filterwarnings("ignore", category=DeprecationWarning, module="faiss.loader")
-warnings.filterwarnings("ignore", message="builtin type SwigPyPacked has no __module__ attribute")
-warnings.filterwarnings("ignore", message="builtin type SwigPyObject has no __module__ attribute")
-warnings.filterwarnings("ignore", message="builtin type swigvarlink has no __module__ attribute")
